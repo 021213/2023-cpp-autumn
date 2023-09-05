7bacf57785598e38ab5932b9cdafebbf5c4e9a93
021213 <st021213@spbu.ru>
2023-09-05 23:02:17 +0300

    Lesson 1 code update

--- 2023.09.05-Lesson-1/Project2/Source.cpp
+++ 2023.09.05-Lesson-1/Project2/Source.cpp
@@ -4,6 +4,6 @@ int main(int argc, char* argv[])
 {
 	int a = 0;
 	std::cin >> a;
-	std::cout << a / 100 + a / 10 % 10 + a % 10 << std::endl;
+	std::cout << a % 10  + a / 10 % 10 + a / 100 << std::endl;
 	return EXIT_SUCCESS;
 }
