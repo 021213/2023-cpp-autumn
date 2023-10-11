ea2e458e32d0686843f0ea4179f9725d0da4683a
021213 <st021213@spbu.ru>
2023-10-11 23:10:32 +0300

    aquarius code upload

--- 2023.10.11-Test-1(prep)/Homework-3/Source.cpp
+++ 2023.10.11-Test-1(prep)/Homework-3/Source.cpp
@@ -59,3 +59,82 @@ int main(int argc, char* argv[])
 	}
 	return EXIT_SUCCESS;
 }
+
+int main1()
+{
+	int a, b, n, s1, s2, possible = 1;
+	std::cin >> a >> b >> n;
+	s1 = 0;
+	s2 = 0;
+	while (true) {
+		if (s1 == 0)
+		{
+			s1 += a;
+			if (s1 == n || s2 == n) break;
+		}
+		if (s1 < b - s2)
+		{
+			s2 += s1;
+			s1 = 0;
+		}
+		else
+		{
+			s1 -= b - s2;
+			s2 = b;
+		}
+		if (s1 == n || s2 == n) break;
+		if (s2 == b)
+		{
+			s2 = 0;
+			if (s1 == n || s2 == n) break;
+		}
+		if (s1 == 0 && s2 == 0)
+		{
+			possible = 0;
+			break;
+		}
+	}
+
+
+	if ((a < n && b < n) || (possible == 0))
+	{
+		std::cout << "Impossible" << std::endl;
+	}
+	else
+	{
+		s1 = 0;
+		s2 = 0;
+		while (true) {
+			if (s1 == 0)
+			{
+				s1 += a;
+				std::cout << ">A" << std::endl;
+				if (s1 == n || s2 == n) return 0;
+			}
+			if (s1 < b - s2)
+			{
+				s2 += s1;
+				s1 = 0;
+			}
+			else
+			{
+				s1 -= b - s2;
+				s2 = b;
+			}
+			std::cout << "A>B" << std::endl;
+			if (s1 == n || s2 == n) return 0;
+			if (s2 == b)
+			{
+				s2 = 0;
+				std::cout << "B>" << std::endl;
+				if (s1 == n || s2 == n) return 0;
+			}
+			if (s1 == 0 && s2 == 0)
+			{
+				std::cout << "Impossible" << std::endl;
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
